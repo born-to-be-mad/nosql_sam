3e5d80db9f269479153bbfa1cfc602e3616369ed
Sergiy Morenets <sergey.morenets@gmail.com>
2020-06-26 16:32:13 +0300

    Task #8

--- nosql-mongo/src/main/java/it/discovery/nosql/MongoApplication.java
+++ nosql-mongo/src/main/java/it/discovery/nosql/MongoApplication.java
@@ -1,19 +1,19 @@
 package it.discovery.nosql;
 
-import it.discovery.nosql.repository.BookRepository;
 import it.discovery.nosql.service.BookService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.MongoOperations;
 
 @SpringBootApplication
 @EnableMongoAuditing
 public class MongoApplication {
 
     @Bean
-    public BookService bookService(BookRepository bookRepository) {
-        return new BookService(bookRepository);
+    public BookService bookService(MongoOperations mongoOperations) {
+        return new BookService(mongoOperations);
     }
 
     public static void main(String[] args) {
--- nosql-mongo/src/main/java/it/discovery/nosql/service/BookService.java
+++ nosql-mongo/src/main/java/it/discovery/nosql/service/BookService.java
@@ -1,21 +1,23 @@
 package it.discovery.nosql.service;
 
 import it.discovery.nosql.model.Book;
-import it.discovery.nosql.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 public class BookService {
 
-	private final BookRepository bookRepository;
+    private final MongoOperations mongoOperations;
 
-	public List<Book> findByName(String name) {
-		return bookRepository.findByName(name);
-	}
+    public List<Book> findByName(String name) {
+        return mongoOperations.find(new Query(Criteria.where("names.name").is(name)), Book.class);
+    }
 
-	public void saveBook(Book book) {
-		bookRepository.save(book);
-	}
+    public void saveBook(Book book) {
+        mongoOperations.save(book);
+    }
 }
