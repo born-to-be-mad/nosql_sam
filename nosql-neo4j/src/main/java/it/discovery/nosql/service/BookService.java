1fa344cc5efafdcc2654d8956de3e561fec6bad8
Sergiy Morenets <sergey.morenets@gmail.com>
2020-06-27 16:21:07 +0300

    Task 18

--- nosql-neo4j/src/main/java/it/discovery/nosql/service/BookService.java
+++ nosql-neo4j/src/main/java/it/discovery/nosql/service/BookService.java
@@ -3,21 +3,17 @@ package it.discovery.nosql.service;
 import it.discovery.nosql.model.Book;
 import it.discovery.nosql.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
-
-@Service
 @RequiredArgsConstructor
 public class BookService {
 
-	private final BookRepository bookRepository;
+    private final BookRepository bookRepository;
 
-	public List<Book> findByName(String name) {
-		return bookRepository.findByName(name);
-	}
+//    public List<Book> findByName(String name) {
+//        return bookRepository.findByName(name);
+//    }
 
-	public void saveBook(Book book) {
-		bookRepository.save(book);
-	}
+    public void saveBook(Book book) {
+        bookRepository.save(book);
+    }
 }
